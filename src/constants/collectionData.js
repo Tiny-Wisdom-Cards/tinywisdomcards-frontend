7651f621867d2821afecf3ef1f0d3b6c92600bb1
Samir Lamsal <samirlamsal5@gmail.com>
2025-08-20 13:33:17 +0545

    price changed to 499

--- src/constants/collectionData.js
+++ src/constants/collectionData.js
@@ -6,7 +6,7 @@ export const COLLECTIONDATA = [
     description:
       "The complete collection. 31 cards and a guide book in a keepsake box.",
     price: "350 NPR",
-    originalPrice: "400 NPR",
+    originalPrice: "499 NPR",
     dollor_price:"$19.99",
     dollorOriginalPrice: "$25",
     buttonText: "Prebook Now",
